83a7e216d2cfac0be19ad3be4097ec4807fcddb6
ProgramFOX <programfox@hotmail.be>
2017-09-14 22:41:17 +0200

    Add 'commands' and 'help'

--- commands.py
+++ commands.py
@@ -6,7 +6,7 @@ class Commands():
     """Commands for ChessBot"""
     def __init__(self):
         self.openings = openings.Openings("eco/eco.json")
-        self.commands = {"opening": self.opening}
+        self.commands = {"opening": self.opening, "commands": self.cmds, "help": self.help}
 
     def execute(self, cmd):
         """Executes a command from chat."""
@@ -27,3 +27,13 @@ class Commands():
             return "No matching opening found."
         return "**{0} {1}**. Moves: `{2}`. Board: <https://lichess.org/analysis/{3}>"\
             .format(found.eco, found.name, found.as_pgn_moves(), found.fen.replace(" ", "_"))
+
+    def cmds(self, _):
+        """`commands` command"""
+        return "Commands: {}".format(", ".join(
+            sorted(map(lambda x: "`" + x + "`", self.commands.keys()))
+        ))
+
+    def help(self, _):
+        """`help` command"""
+        return "Hello! I'm a bot, created by ProgramFOX. Use `commands` to see what I can do!"
